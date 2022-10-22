5e747741ab16cbd41cba2340ec8d1e690ffafcf9
Joytaylor <jtaylor2@imsa.edu>
2022-10-22 05:28:39 -0500

    adding componets

--- my-app/views/addTime.js
+++ my-app/views/addTime.js
@@ -1,17 +1,28 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Grid from "@mui/material/Grid";
 
-import ButtonGroup from '@mui/material/ButtonGroup';
+export default function TimeMenu(props) {
+  return (
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button>30</Button>
+        <Button>45</Button>
+        <Button>60</Button>
+      </ButtonGroup>
+      <Button> End Reservation</Button>
 
-export default function TimeMenu(props){
-  return(
-    <div>
-<ButtonGroup variant="contained" aria-label="outlined primary button group">
-  <Button>30</Button>
-  <Button>45</Button>
-  <Button>60</Button> // buttons should automatically add time to the total
-</ButtonGroup>
-<Button> End Reservation</Button>
-<h6> {props.text} minutes left </h6>
-</div> );
+      <p> {props.text} minutes left </p>
+    </Grid>
+  );
 }
